eebbbb96274f205d7948bc84363cde9ffb630a3d
David <bi-crosseur@hotmail.fr>
2019-04-10 13:10:22 +0200

    Add a living time to bullets

--- Assets/Scripts/BulletMover.cs
+++ Assets/Scripts/BulletMover.cs
@@ -4,11 +4,24 @@ public class BulletMover : MonoBehaviour
 {
 
     public float Speed;
+    // Time in seconds before the bullet is destroyed
+    public float LivingTime;
+
+    private float m_spawnTime;
 
     public void Start() {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         // Move the bullet straight ahead with constant speed
         rigidbody.velocity = transform.forward * Speed;
+
+        m_spawnTime = Time.time;
+    }
+
+    public void Update() {
+        // Destroy the bullet when the LivingTime is elapsed
+        if(Time.time - m_spawnTime > LivingTime) {
+            Destroy(this.gameObject);
+        }
     }
 
     public void OnCollisionEnter(Collision collision) {
