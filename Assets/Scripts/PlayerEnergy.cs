a9244bb8dbafa39c08a46783943c8b02025cfb79
ricebal <ricebalvids@gmail.com>
2019-04-10 13:32:35 +0200

    Added energy bar

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnergy : MonoBehaviour
+{
+    public int StartingEnergy = 100;
+    public int CurrentEnergy;
+    public Slider EnergySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentEnergy = StartingEnergy;
+        EnergySlider = GameObject.Find("EnergySlider").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")) {
+            AddEnergy(-10);
+        }
+        if(Input.GetKeyDown("return")) {
+            AddEnergy(10);
+        }
+        EnergySlider.value = CurrentEnergy;
+    }
+
+    void AddEnergy(int value) {
+        CurrentEnergy += value;
+        if(CurrentEnergy > 100)
+            CurrentEnergy = 100;   
+        if(CurrentEnergy < 0)
+            CurrentEnergy = 0;
+    }
+}
