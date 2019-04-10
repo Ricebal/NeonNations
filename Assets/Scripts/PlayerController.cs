76fdf545ce39aa2b44727bf1165f79e90eaaea6c
Blueriin <Stella.gra@hotmail.fr>
2019-04-10 09:49:01 +0200

    Add basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody m_rigidBody;
+
+    // Start is called before the first frame update
+    public void Start() {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        m_rigidBody.velocity = movement * Speed;
+    }
+}
