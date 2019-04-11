55ffa0a3f0eaa330f0321147fe14327e8bb4c19a
ricebal <ricebalvids@gmail.com>
2019-04-11 13:47:29 +0200

    Added aiming to player

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Aim : NetworkBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isLocalPlayer)
+            return;
+        Vector3 v3T = Input.mousePosition;
+        v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        v3T = Camera.main.ScreenToWorldPoint(v3T);
+        v3T -= transform.position;
+        v3T = v3T * 10000.0f + transform.position;
+        transform.LookAt(v3T);    
+    }
+}
