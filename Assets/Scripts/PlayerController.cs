a72f73f87633290933a6e66cdccdcb5a1c2b0747
Blueriin <Stella.gra@hotmail.fr>
2019-04-11 10:24:21 +0200

    Update PlayerController
    The player moves direclty when a key is pressed and stops moving when the key is released. Still a collision problem.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,18 +18,18 @@ public class PlayerController : NetworkBehaviour
         if(!isLocalPlayer)
             return;
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
         MovePlayer(moveHorizontal, moveVertical);
     }
 
     public void MovePlayer(float horizontal, float vertical) {
         m_movement.Set(horizontal, 0, vertical);
 
-        if (horizontal != 0 || vertical != 0) {
+        /*if (horizontal != 0 || vertical != 0) {
             m_rigidBody.MoveRotation(Quaternion.LookRotation(m_movement));
         }
-
+        */
         m_movement = m_movement.normalized * Speed * Time.deltaTime;
         m_rigidBody.MovePosition(transform.position + m_movement);
     }
