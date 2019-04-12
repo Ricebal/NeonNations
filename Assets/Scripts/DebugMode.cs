eced1c7976e8f6acdf7ba86d029437471e541343
ricebal <ricebalvids@gmail.com>
2019-04-12 13:55:20 +0200

    Added debug

---
+++ Assets/Scripts/DebugMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMode : MonoBehaviour
+{
+    public GameObject DirectionalLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        DirectionalLight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            DirectionalLight.SetActive(!DirectionalLight.activeSelf);
+        }
+    }
+}
