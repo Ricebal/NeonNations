1e52baa8d1dfc01b4850e080381d4212928940fb
Blueriin <Stella.gra@hotmail.fr>
2019-04-12 10:11:54 +0200

    Fixing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,15 @@ public class Player : NetworkBehaviour
     public int BulletCost;
     // Amount of energy a sonar will consume
     public int SonarCost;
+    // Fire rate in seconds
+    public float FireRate;
+    // Sonar rate in seconds
+    public float SonarRate;
+
+    // The next time the entity will be able to shoot, in seconds
+    private float m_nextFire;
+    // The next time the entity will be able to use the sonar, in seconds
+    private float m_nextSonar;
 
     private PlayerAction m_playerAction;
     private PlayerEnergy m_playerEnergy;
@@ -33,7 +42,8 @@ public class Player : NetworkBehaviour
 
     public void Shoot() {
         // If the cooldown is elapsed and the player has enough energy
-        if (Time.time > m_playerAction.getNextFire() && m_playerEnergy.CurrentEnergy >= BulletCost) {
+        if (Time.time > m_nextFire && m_playerEnergy.CurrentEnergy >= BulletCost) {
+            m_nextFire = Time.time + FireRate;
             m_playerEnergy.AddEnergy(-BulletCost);
             m_playerAction.CmdShoot();
         }
@@ -41,7 +51,8 @@ public class Player : NetworkBehaviour
 
     public void Sonar() {
         // If the cooldown is elapsed and the player has enough energy
-        if (Time.time > m_playerAction.getNextSonar() && m_playerEnergy.CurrentEnergy >= SonarCost) {
+        if (Time.time > m_nextSonar && m_playerEnergy.CurrentEnergy >= SonarCost) {
+            m_nextSonar = Time.time + SonarRate;
             m_playerEnergy.AddEnergy(-SonarCost);
             m_playerAction.CmdSonar();
         }
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -10,20 +10,9 @@ public class PlayerAction : NetworkBehaviour
     // Prefab representing the sonar bullet
     public GameObject SonarBullet;
     public Transform BulletSpawn;
-    // Fire rate in seconds
-    public float FireRate;
-    // Sonar rate in seconds
-    public float SonarRate;
-
-    // The next time the entity will be able to shoot, in seconds
-    private float m_nextFire;
-    // The next time the entity will be able to use the sonar, in seconds
-    private float m_nextSonar;
 
     [Command]
     public void CmdShoot() {
-        m_nextFire = Time.time + FireRate;
-
         GameObject bullet = Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation) as GameObject;
         bullet.GetComponent<BulletMover>().SetShooter(this.gameObject);
 
@@ -33,8 +22,6 @@ public class PlayerAction : NetworkBehaviour
 
     [Command]
     public void CmdSonar() {
-        m_nextSonar = Time.time + SonarRate;
-
         for (int i = 0; i < 360; i += 2) {
             GameObject sonarBullet = Instantiate(SonarBullet, this.transform.position, BulletSpawn.rotation * Quaternion.Euler(0.0f, i, 0.0f)) as GameObject;
             sonarBullet.GetComponent<BulletMover>().SetShooter(this.gameObject);
@@ -42,14 +29,6 @@ public class PlayerAction : NetworkBehaviour
             // Instanciate the bullet on the network for all players 
             NetworkServer.Spawn(sonarBullet);
         }
-    }
-
-    public float getNextFire() {
-        return m_nextFire;
-    }
-
-    public float getNextSonar() {
-        return m_nextSonar;
     }
 
 }
