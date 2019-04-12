51921fd86cb5921b566086ef1796dfb5b45d8ca6
ricebal <ricebalvids@gmail.com>
2019-04-12 14:24:37 +0200

    Optimization PogChamp

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -8,11 +8,11 @@ public class LightController : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(isLocalPlayer)
+        if(!isLocalPlayer)
             return;
 
-        transform.GetChild(1).gameObject.SetActive(false);
-        transform.GetChild(2).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(2).gameObject.SetActive(true);
     }
 
     // Update is called once per frame
