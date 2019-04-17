f9744e0cde3af24b25c970177397dd044f9f1fca
Blueriin <Stella.gra@hotmail.fr>
2019-04-17 10:28:11 +0200

    Add lobby scene and create NetworkManager file

---
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkManagerCustom : NetworkManager
+{
+    public void SartUpHost() {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame() {
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+    }
+
+    void SetIPAddress() {
+        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+
+    void SetPort() {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+}
