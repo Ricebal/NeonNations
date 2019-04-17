8928b013bcd3537ece5c0a1c0959a21f3c5c9b89
Benji3199 <benji3199@gmail.com>
2019-04-17 19:15:59 +0200

    So... some weird branch stuff happend, but this is now the improved version of bullet_impact. Last thing to search for is to make the point light a hemisphere to prevent it from shining through the walls.

--- Assets/Scripts/Bullet/ExplosionLight.cs
+++ Assets/Scripts/Bullet/ExplosionLight.cs
@@ -8,18 +8,33 @@ public class ExplosionLight : MonoBehaviour
     public float IntensityMultiplier;
     public float Lifetime;
     public Light Light;
+    private bool _growing = true;
+    private float _timeCount = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         //Destroys explosion after certain time
-        Destroy(gameObject, Lifetime);
+        Destroy(gameObject, Lifetime*2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Light.range += RangeMultiplier * Time.deltaTime;
-        Light.intensity += IntensityMultiplier * Time.deltaTime;
+        _timeCount += Time.deltaTime;
+        if(_timeCount > Lifetime)
+        {
+            _growing = false;
+        }
+        if (_growing)
+        {
+            Light.range += RangeMultiplier * Time.deltaTime;
+            Light.intensity += IntensityMultiplier * Time.deltaTime;
+        }
+        else
+        {
+            Light.range -= RangeMultiplier * Time.deltaTime;
+            Light.intensity -= IntensityMultiplier * Time.deltaTime;
+        }
     }
 }
