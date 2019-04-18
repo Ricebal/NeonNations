76168252babb6a5c8999cb09417bf9fd608ceed6
Blueriin <Stella.gra@hotmail.fr>
2019-04-18 11:59:26 +0200

    Add newtork menu
    Add a NetworkMenu script to handle the buttons separately from the NetworkManagerCustom script, which takes care of the network only.

--- Assets/Scripts/NetworkManagerCustom.cs
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -3,40 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class NetworkManagerCustom : NetworkManager
 {
-    void OnEnable() {
-        SceneManager.sceneLoaded += OnLobbyLoaded;
-    }
 
-    void OnDisable() {
-        SceneManager.sceneLoaded -= OnLobbyLoaded;
-    }
-
-    private void OnLobbyLoaded(Scene scene, LoadSceneMode mode) {
-        if (scene.name == "Lobby") {
-            GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
-            GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(StartUpHost);
-
-            GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
-            GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(JoinGame);
-        }
-    }
+    // Start a game as a host
     public void StartUpHost() {
         SetPort();
         NetworkManager.singleton.StartHost();
     }
 
+    // Join a game as a client
     public void JoinGame() {
         SetIPAddress();
         SetPort();
         NetworkManager.singleton.StartClient();
     }
 
+    // Set the IP address of the network manager for the StartClient function
     void SetIPAddress() {
-        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.FindChild("Text").GetComponent<Text>().text;
+        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
         NetworkManager.singleton.networkAddress = ipAddress;
     }
 
---
+++ Assets/Scripts/NetworkMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NetworkMenu : MonoBehaviour
+{
+    public Button ButtonStartHost;
+    public Button ButtonJoingGame;
+    public Button ButtonBack;
+
+    private NetworkManagerCustom m_networkManagerCustom;
+
+    void Awake() {
+        m_networkManagerCustom = GameObject.Find("NetworkManager").GetComponent<NetworkManagerCustom>();
+    }
+
+    void OnEnable() {
+        SceneManager.sceneLoaded += OnNetworkMenuLoaded;
+    }
+
+    void OnDisable() {
+        SceneManager.sceneLoaded -= OnNetworkMenuLoaded;
+    }
+
+    private void OnNetworkMenuLoaded(Scene scene, LoadSceneMode mode) {
+        // If the NetworkMenu scene is loaded...
+        if (scene.name == "NetworkMenu") {
+            // if the button start host is clicked, call the StartUpHost function
+            ButtonStartHost.onClick.RemoveAllListeners();
+            ButtonStartHost.onClick.AddListener(m_networkManagerCustom.StartUpHost);
+
+            // if the button join game is clicked, call the JoinGame function
+            ButtonJoingGame.onClick.RemoveAllListeners();
+            ButtonJoingGame.onClick.AddListener(m_networkManagerCustom.JoinGame);
+
+            // if the button start host is clicked, call the LoadMainMenu function
+            ButtonBack.onClick.RemoveAllListeners();
+            ButtonBack.onClick.AddListener(LoadMainMenu);
+        }
+    }
+
+    // Go to the MainMenu scene
+    private void LoadMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
