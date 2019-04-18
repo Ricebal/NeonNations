65a14ce330d5955533dd9b9b6405b061d38ebde4
David <bi-crosseur@hotmail.fr>
2019-04-18 14:50:17 +0200

    Fix Play button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Play game button
-    public void PlayGame()
-    {
+    public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Quit game button
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Application.Quit();
     }
 }
