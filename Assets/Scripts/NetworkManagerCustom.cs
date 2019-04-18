812c777a0812d22742e1cf20144cd010593d26d1
Blueriin <Stella.gra@hotmail.fr>
2019-04-18 10:18:05 +0200

    Modify NetworkManagerCustom
    Joining game now works

--- Assets/Scripts/NetworkManagerCustom.cs
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -7,23 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class NetworkManagerCustom : NetworkManager
 {
-    void OnEnable() {
-        SceneManager.sceneLoaded += OnLobbyLoaded;
-    }
-
-    void OnDisable() {
-        SceneManager.sceneLoaded -= OnLobbyLoaded;
-    }
-
-    private void OnLobbyLoaded(Scene scene, LoadSceneMode mode) {
-        if (scene.name == "Lobby") {
-            GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
-            GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(StartUpHost);
-
-            GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
-            GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(JoinGame);
-        }
-    }
 
     public void StartUpHost() {
         SetPort();
