64f53bb04b823862a07fce309cbd018d0abee25a
David <bi-crosseur@hotmail.fr>
2019-04-25 15:53:25 +0200

    Fix connection errors handling

--- Assets/Scripts/NetworkManagerCustom.cs
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -11,7 +11,6 @@ public class NetworkManagerCustom : NetworkManager
     {
         base.Start();
 
-        //connectionConfig.MaxConnectionAttempt = 2;
         m_isConnecting = false;
         m_connectionText = "";
     }
@@ -24,7 +23,6 @@ public class NetworkManagerCustom : NetworkManager
             m_connectionText = "Disconnecting";
             StopClient();
         }
-        SetPort();
         StartHost();
     }
 
@@ -37,32 +35,26 @@ public class NetworkManagerCustom : NetworkManager
             m_connectionText = "Connecting...";
 
             SetIPAddress();
-            SetPort();
             StartClient();
         }
     }
 
-    public override void OnClientError(NetworkConnection conn, int errorCode)
+    public override void OnClientDisconnect(NetworkConnection conn)
     {
-        // If the client is not properly disconnected...
-        if ((int) UnityEngine.Networking.NetworkError.Ok != errorCode)
-        {
-            // and if it is a timeout error, print "connection failed"
-            if ((int) UnityEngine.Networking.NetworkError.Timeout == errorCode)
-            {
-                m_connectionText = "Connection failed";
-            }
-            // print the error in the console
-            Debug.LogError("ClientDisconnected due to error: " + (UnityEngine.Networking.NetworkError) errorCode);
-        }
+        base.OnClientDisconnect(conn);
 
-        Debug.Log("Client disconnected from server: " + conn);
-        m_isConnecting = false;
+        // If the client is disconnected during a connection attempt, an error occurred
+        if(m_isConnecting)
+        {
+            m_connectionText = "An error occurred while connecting";
+            m_isConnecting = false;
+        }
     }
 
     public override void OnClientConnect(NetworkConnection conn)
     {
         base.OnClientConnect(conn);
+
         m_connectionText = "";
         m_isConnecting = false;
     }
@@ -72,12 +64,6 @@ public class NetworkManagerCustom : NetworkManager
     {
         string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
         NetworkManager.singleton.networkAddress = ipAddress;
-    }
-
-    // Set the port of the network manager for the StartClient function
-    void SetPort()
-    {
-       // NetworkManager.singleton.networkPort = 7777;
     }
 
     public bool IsConnecting()
