18064fb246dbc5dc269e686ec45b09df9a1a926f
David <bi-crosseur@hotmail.fr>
2019-05-07 21:12:53 +0200

    Improve player's rotation synchronization

---
+++ Assets/Scripts/Soldier/SyncRotation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncRotation : NetworkBehaviour
+{
+    public float LerpRate = 15;
+    public Transform Transform;
+
+    [SyncVar]
+    private Quaternion m_syncPlayerRotation;
+
+    void FixedUpdate()
+    {
+        TransmitRotation();
+        LerpRotation();
+    }
+
+    private void LerpRotation()
+    {
+        if(!isLocalPlayer)
+        {
+            Transform.rotation = Quaternion.Lerp(Transform.rotation, m_syncPlayerRotation, Time.deltaTime * LerpRate);
+        }
+    }
+
+    [Command]
+    private void CmdProvideRotationToServer(Quaternion playerRotation)
+    {
+        m_syncPlayerRotation = playerRotation;
+    }
+
+    [Client]
+    private void TransmitRotation()
+    {
+        if(isLocalPlayer)
+        {
+            CmdProvideRotationToServer(Transform.rotation);
+        }
+    }
+}
