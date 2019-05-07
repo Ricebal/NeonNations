8468d55dcc3edb1d2aa3cce5088a0dd2d79f3698
David <bi-crosseur@hotmail.fr>
2019-05-07 20:21:52 +0200

    Immprove player's movement synchronization
    By replacing NetworkTransform by a custom script

---
+++ Assets/Scripts/Soldier/SyncPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncPosition : NetworkBehaviour
+{
+    public float LerpRate = 15;
+    public Transform Transform;
+
+    [SyncVar]
+    private Vector3 m_syncPos;
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    private void LerpPosition()
+    {
+        if(!isLocalPlayer)
+        {
+            Transform.position = Vector3.Lerp(Transform.position, m_syncPos, Time.deltaTime * LerpRate);
+        }
+    }
+
+    [Command]
+    private void CmdProvidePositionToServer(Vector3 pos)
+    {
+        m_syncPos = pos;
+    }
+
+    [ClientCallback]
+    private void TransmitPosition()
+    {
+        if(isLocalPlayer)
+        {
+            CmdProvidePositionToServer(Transform.position);
+        }
+    }
+
+}
