a60c25a778c4d4badb83ba596b120f1ec712feb9
Benji3199 <benji3199@gmail.com>
2019-05-09 16:04:50 +0200

    Start for line raycast on map

--- Assets/Scripts/Soldier/Bot/DStar/GameEnvironment.cs
+++ Assets/Scripts/Soldier/Bot/DStar/GameEnvironment.cs
@@ -19,6 +19,63 @@ namespace Assets.Scripts.Soldier.Bot.DStar
 
         }
 
+        //CheckForCollision();
+
+        /// <summary>
+        ///Swap the values of A and B
+        /// </summary>
+        private void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        /// <summary>
+        /// Returns a List of Coordinates which collide with the line between a startpoint and an endpoint
+        /// </summary>
+        /// <param name="startPointX">The x coordinate of the startpoint</param>
+        /// <param name="startPointY">The y coordinate of the startpoint</param>
+        /// <param name="endPointX">The x coordinate of the endpoint</param>
+        /// <param name="endPointY">The y coordinate of the endpoint</param>
+        /// <returns>List of intersecting Coordinates</returns>
+        private List<Coordinates> BresenhamLine(int startPointX, int startPointY, int endPointX, int endPointY)
+        {
+            List<Coordinates> result = new List<Coordinates>();
+
+            // If the differnce in hight of the line is higher than the differnce of width, it will be a steep line.
+            bool steep = Math.Abs(endPointY - startPointY) > Math.Abs(endPointX - startPointX);
+            if (steep)
+            {
+                Swap(ref startPointX, ref startPointY);
+                Swap(ref endPointX, ref endPointY);
+            }
+            if (startPointX > endPointX)
+            {
+                Swap(ref startPointX, ref endPointX);
+                Swap(ref startPointY, ref endPointY);
+            }
+
+            int deltax = endPointX - startPointX;
+            int deltay = Math.Abs(endPointY - startPointY);
+            int error = 0;
+            int ystep;
+            int y = startPointY;
+            if (startPointY < endPointY) ystep = 1; else ystep = -1;
+            for (int x = startPointX; x <= endPointX; x++)
+            {
+                if (steep) result.Add(new Coordinates(y, x));
+                else result.Add(new Coordinates(x, y));
+                error += deltay;
+                if (2 * error >= deltax)
+                {
+                    y += ystep;
+                    error -= deltax;
+                }
+            }
+            return result;
+        }
+
         /// <summary>
         /// Checks if it's possible to travel straight from one note to the other
         /// </summary>
