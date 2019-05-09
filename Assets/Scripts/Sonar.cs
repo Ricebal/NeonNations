9be240997d1783d7365905017d64ba8af31cecb3
ricebal <ricebalvids@gmail.com>
2019-05-09 14:03:52 +0200

    Fixes

--- Assets/Scripts/Sonar.cs
+++ Assets/Scripts/Sonar.cs
@@ -14,7 +14,7 @@ public class Sonar : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Find("Particle System").parent = null;
+        // transform.Find("Particle System").parent = null;
         m_light.intensity = 0;
         m_light.range = 0;
         m_growing = true;
