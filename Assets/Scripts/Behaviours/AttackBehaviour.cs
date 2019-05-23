85d248304c19c84e58b34b344c84e32f68e8a7dc
ricebal <ricebalvids@gmail.com>
2019-05-23 16:02:30 +0200

    Sort of LoS

--- Assets/Scripts/Behaviours/AttackBehaviour.cs
+++ Assets/Scripts/Behaviours/AttackBehaviour.cs
@@ -8,6 +8,7 @@ public class AttackBehaviour : BotBehaviour
     private Action m_action;
     private Bot m_bot;
     private TeamManager m_teamManager;
+    private const float VISION_RANGE = 5;
 
     private void Start()
     {
@@ -28,18 +29,32 @@ public class AttackBehaviour : BotBehaviour
     private void FireAtClosestEnemy()
     {
         List<Soldier> enemies = m_teamManager.GetEnemiesByTeam(m_bot.Team);
-        Vector2 targetPosition = FindClosestEnemyPosition(enemies);
+        Vector3 targetPosition = FindClosestEnemyPosition(enemies);
+        Vector3 rayCastTarget = targetPosition - transform.position;
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, rayCastTarget, VISION_RANGE);
+        bool canFire = true;
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag != "Player" && hit.collider.tag != "Bullet")
+            {
+                canFire = false;
+                return;
+            }
+        }
+        if (!canFire)
+        {
+            return;
+        }
         FireAtPosition(targetPosition);
     }
 
-    private void FireAtPosition(Vector2 position)
+    private void FireAtPosition(Vector3 position)
     {
-        m_bot.Aim(position);
+        m_bot.Aim(new Vector2(position.x, position.z));
         m_action.Shoot();
-
     }
 
-    private Vector2 FindClosestEnemyPosition(List<Soldier> enemies)
+    private Vector3 FindClosestEnemyPosition(List<Soldier> enemies)
     {
         Vector3 currentPosition = transform.position;
         float minDist = Mathf.Infinity;
@@ -55,6 +70,6 @@ public class AttackBehaviour : BotBehaviour
             }
         });
 
-        return new Vector2(closestEnemyPosition.x, closestEnemyPosition.z);
+        return closestEnemyPosition;
     }
 }
