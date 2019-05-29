8f9fb0536b9bb8a823ca4a845ea3cb7d8de083b2
David <bi-crosseur@hotmail.fr>
2019-05-29 14:29:28 +0200

    Add menu and game music

---
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,42 @@
+﻿using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_menuAudioSource;
+    [SerializeField] private AudioSource m_gameAudioSource;
+    [Scene] [SerializeField] private List<string> m_menuScenes;
+    [Scene] [SerializeField] private List<string> m_gameScenes;
+
+    // Instantiate the MusicManager when the game starts
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnGameStart()
+    {
+        Object obj = Resources.Load("MusicManager");
+        GameObject musicManager = Instantiate(obj) as GameObject;
+        // Rename MusicManager(Clone) to MusicManager
+        musicManager.name = obj.name;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+    private void OnSceneChanged(Scene previousScene, Scene newScene)
+    {
+        if (m_menuScenes.Contains(newScene.name) && !m_menuAudioSource.isPlaying)
+        {
+            m_menuAudioSource.Play();
+            m_gameAudioSource.Stop();
+        }
+        else if (m_gameScenes.Contains(newScene.name) && !m_gameAudioSource.isPlaying)
+        {
+            m_menuAudioSource.Stop();
+            m_gameAudioSource.Play();
+        }
+    }
+}
