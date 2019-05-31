166fa3c29cfa5ba36ce00803d5b1dd82d801ef53
Benji3199 <benji3199@gmail.com>
2019-05-31 22:22:20 +0200

    Fixed most of the lightning bugs. Last thing to do is to activate the spotlight of other players for a short period when they're hit by a bullet directly.

--- Assets/Scripts/Soldier/Effect/ExplosionLight.cs
+++ Assets/Scripts/Soldier/Effect/ExplosionLight.cs
@@ -2,10 +2,11 @@
 
 public class ExplosionLight : MonoBehaviour
 {
-    public float RangeMultiplier;
-    public float IntensityMultiplier;
-    public float Lifetime;
-    public Light Light;
+    [SerializeField] private float m_rangeMultiplier;
+    [SerializeField] private float m_intensityMultiplier;
+    [SerializeField] private float m_lifetime;
+    [SerializeField] private Light m_lightForMap;
+    [SerializeField] private Light m_lightForSoldiers;
 
     private bool m_growing = true;
     private float m_timeCount = 0.0f;
@@ -13,31 +14,35 @@ public class ExplosionLight : MonoBehaviour
     void Start()
     {
         //Destroys explosion after certain time
-        Destroy(gameObject, Lifetime * 2);
+        Destroy(gameObject, m_lifetime * 2);
     }
 
     void Update()
     {
         m_timeCount += Time.deltaTime;
-        if (m_timeCount > Lifetime && m_growing)
+        if (m_timeCount > m_lifetime && m_growing)
         {
             m_growing = false;
         }
         if (m_growing)
         {
-            Light.range += RangeMultiplier * Time.deltaTime;
-            Light.intensity += IntensityMultiplier * Time.deltaTime;
+            m_lightForMap.range += m_rangeMultiplier * Time.deltaTime;
+            m_lightForMap.intensity += m_intensityMultiplier * Time.deltaTime;
+            m_lightForSoldiers.range += m_rangeMultiplier * Time.deltaTime;
+            m_lightForSoldiers.intensity += m_intensityMultiplier * Time.deltaTime;
         }
         else
         {
-            Light.range -= RangeMultiplier * Time.deltaTime;
-            Light.intensity -= IntensityMultiplier * Time.deltaTime;
+            m_lightForMap.range -= m_rangeMultiplier * Time.deltaTime;
+            m_lightForMap.intensity -= m_intensityMultiplier * Time.deltaTime;
+            m_lightForSoldiers.range -= m_rangeMultiplier * Time.deltaTime;
+            m_lightForSoldiers.intensity -= m_intensityMultiplier * Time.deltaTime;
         }
     }
 
     public void SetColor(Color color)
     {
-        Light.color = color;
+        m_lightForMap.color = color;
         GetComponentsInChildren<ParticleSystemRenderer>()[0].trailMaterial.SetColor("_EmissionColor", color * 3); ParticleSystemRenderer[] particleSystemRenderers = GetComponentsInChildren<ParticleSystemRenderer>();
 
         // Make new material.
--- Assets/Scripts/Soldier/Sonar.cs
+++ Assets/Scripts/Soldier/Sonar.cs
@@ -2,7 +2,8 @@
 
 public class Sonar : MonoBehaviour
 {
-    [SerializeField] private Light m_light;
+    [SerializeField] private Light m_lightForMap;
+    [SerializeField] private Light m_lightForSoldiers;
     [SerializeField] private float m_maxIntensity;
     [SerializeField] private float m_maxRange;
     [SerializeField] private float m_lifeSpan;
@@ -11,14 +12,12 @@ public class Sonar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_light.intensity = 0;
-        m_light.range = 0;
         m_growing = true;
     }
 
     public void SetColor(Color color)
     {
-        m_light.color = color;
+        m_lightForMap.color = color;
         // color times three for intensity
         ParticleSystemRenderer particleSystemRenderer = GetComponentInChildren<ParticleSystemRenderer>();
         Material mat = Material.Instantiate(particleSystemRenderer.trailMaterial);
@@ -33,7 +32,7 @@ public class Sonar : MonoBehaviour
         float intensityAmount = Time.deltaTime / m_lifeSpan * m_maxIntensity * 2f;
         float rangeAmount = Time.deltaTime / m_lifeSpan * m_maxRange * 2f;
 
-        if (m_light.intensity > m_maxIntensity && m_light.range > m_maxRange)
+        if (m_lightForMap.intensity > m_maxIntensity && m_lightForMap.range > m_maxRange)
         {
             m_growing = false;
         }
@@ -41,17 +40,22 @@ public class Sonar : MonoBehaviour
         if (m_growing)
         {
             // Times two because it should grow faster
-            m_light.intensity += intensityAmount * 2f;
-            m_light.range += rangeAmount * 2f;
+            m_lightForMap.intensity += intensityAmount * 2f;
+            m_lightForMap.range += rangeAmount * 2f;
+            m_lightForSoldiers.intensity += intensityAmount * 2f;
+            m_lightForSoldiers.range += rangeAmount * 2f;
+            
         }
         else
         {
             // Times 0.75 because it should shrink slower
-            m_light.intensity -= intensityAmount * 0.75f;
-            m_light.range -= rangeAmount * 0.75f;
+            m_lightForMap.intensity -= intensityAmount * 0.75f;
+            m_lightForMap.range -= rangeAmount * 0.75f;
+            m_lightForSoldiers.intensity -= intensityAmount * 0.75f;
+            m_lightForSoldiers.range -= rangeAmount * 0.75f;
         }
 
-        if (m_light.intensity <= 0 && m_light.range <= 0)
+        if (m_lightForMap.intensity <= 0 && m_lightForMap.range <= 0)
         {
             Destroy(this.gameObject);
         }
