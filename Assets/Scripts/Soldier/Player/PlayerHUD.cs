a3cd6a5871cac5e08985a3b806788b14165d845a
Nicander Mohrmann <s1103064@student.windesheim.nl>
2019-05-31 13:01:56 +0200

    Fixes

--- Assets/Scripts/Soldier/Player/PlayerHUD.cs
+++ Assets/Scripts/Soldier/Player/PlayerHUD.cs
@@ -4,30 +4,11 @@ using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    public static PlayerHUD Singleton;
     [SerializeField] private Stat m_healthStat;
     [SerializeField] private Stat m_energyStat;
     private GameObject m_hud;
     private Slider m_healthSlider;
     private Slider m_energySlider;
-
-    private void Awake()
-    {
-        InitializeSingleton();
-        Singleton.m_hud = GameObject.FindGameObjectWithTag("HUD");
-    }
-
-    private void InitializeSingleton()
-    {
-        if (Singleton != null && Singleton != this)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Singleton = this;
-        }
-    }
 
     private void Start()
     {
@@ -35,14 +16,14 @@ public class PlayerHUD : MonoBehaviour
         m_energySlider = GameObject.Find("EnergySlider").GetComponent<Slider>();
     }
 
-    public static void UpdateHUD()
+    public void UpdateHUD()
     {
-        Singleton.m_healthSlider.value = Singleton.m_healthStat.GetValue();
-        Singleton.m_energySlider.value = Singleton.m_energyStat.GetValue();
+        m_healthSlider.value = Singleton.m_healthStat.GetValue();
+        m_energySlider.value = Singleton.m_energyStat.GetValue();
     }
 
-    public static void SetSeed(string seed)
+    public void SetSeed(string seed)
     {
-        Singleton.m_hud.GetComponent<TextMeshProUGUI>().text = seed;
+        m_hud.GetComponent<TextMeshProUGUI>().text = seed;
     }
 }
