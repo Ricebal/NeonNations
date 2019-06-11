451e8e9c6f70d67d8af36bbd2b0d2f0599428087
David <bi-crosseur@hotmail.fr>
2019-06-11 11:47:07 +0200

    Add moving dots to loading text

--- Assets/Scripts/Menu/LoadingScreen.cs
+++ Assets/Scripts/Menu/LoadingScreen.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class LoadingScreen : MonoBehaviour
 {
     public static LoadingScreen Singleton;
+
+    [SerializeField] private TextMeshProUGUI m_loadingText = null;
+    [SerializeField] private float m_interval = 0;
+    private string m_initialText;
+    private float m_startTime;
 
     private void Awake()
     {
@@ -20,6 +26,24 @@ public class LoadingScreen : MonoBehaviour
             Singleton = this;
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        m_initialText = m_loadingText.text;
+        m_startTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        int dotCount = (int) ((Time.time - m_startTime) / m_interval) % 4;
+
+        string dots = "";
+        for (int i = 0; i < dotCount; i++)
+        {
+            dots += ".";
+        }
+        m_loadingText.SetText(m_initialText + dots);
     }
 
 }
