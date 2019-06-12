fbb8b635faba718ba6a2b4d76306695cb28ac453
ricebal <ricebalvids@gmail.com>
2019-06-12 14:24:07 +0200

    Stat no longer defaults to 0,100

--- Assets/Scripts/Soldier/Soldier.cs
+++ Assets/Scripts/Soldier/Soldier.cs
@@ -10,8 +10,8 @@ public abstract class Soldier : NetworkBehaviour
     public string Username;
     public float RespawnTime;
     public bool IsDead = false;
-    public Stat HealthStat = new Stat(0, 100);
-    public Stat EnergyStat = new Stat(0, 100);
+    public Stat HealthStat;
+    public Stat EnergyStat;
 
     [SerializeField] protected float m_energyReloadTime;
     [SerializeField] protected GameObject m_spotLight;
